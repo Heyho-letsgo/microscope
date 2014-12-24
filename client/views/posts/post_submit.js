6abe3be17a608ca8932ca0f721e80f686d79a387
André Baudin-Laurencin <abaudin@gmail.com>
2014-12-24 13:50:15 +0100

    Utiliser une Method pour envoyer le post.

--- client/views/posts/post_submit.js
+++ client/views/posts/post_submit.js
@@ -1,14 +1,18 @@
 Template.postSubmit.events({
-  'submit form': function(e) {
-    e.preventDefault();
+    'submit form': function (e) {
+        e.preventDefault();
 
-    var post = {
-      url: $(e.target).find('[name=url]').val(),
-      title: $(e.target).find('[name=title]').val(),
-      message: $(e.target).find('[name=message]').val()
+        var post = {
+            url: $(e.target).find('[name=url]').val(),
+            title: $(e.target).find('[name=title]').val(),
+            message: $(e.target).find('[name=message]').val()
+        }
+
+        Meteor.call('post', post, function (error, id) {
+            if (error)
+                return alert(error.reason);
+
+            Router.go('postPage', {_id: id});
+        });
     }
-
-    post._id = Posts.insert(post);
-    Router.go('postPage', post);
-  }
 });
